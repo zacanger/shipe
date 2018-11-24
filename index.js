3c303a33304d02080ae6f0993ea98083637ac20a
zacanger <zac@zacanger.com>
2018-11-24 13:33:18 -0700

    Cleanup

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 #!/usr/bin/env node
 
-const id = (a) => a
-const script = eval(process.argv[2]) || id
-
 const handle = (handlers) => {
   process.stdin.resume()
   process.stdin.on('data', (d) => {
     process.stdout.write(handlers(d.toString()))
-    process.stdout.write('\n')
   })
-  process.stdout.on('error', function epipeFilter(err) {
+
+  process.stdout.on('error', function errorHandler (err) {
     if (err.code === 'EPIPE') return process.exit()
     if (process.stdout.listeners('error').length <= 1) {
       process.stdout.removeAllListeners()
       process.stdout.emit('error', err)
-      process.stdout.on('error', epipeFilter)
+      process.stdout.on('error', errorHandler )
     }
   })
 }
 
-const main = () => {
-  handle(script)
+if (!module.parent) {
+  handle(eval(process.argv[2]) || ((a) => a))
 }
-
-if (!module.parent) main()
