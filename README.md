ba1cb455377c68182eb8aa68a381892af76b0630
zacanger <zac@zacanger.com>
2018-11-24 12:58:18 -0700

    Add the code

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # shipe
 
+Use Node in Shell pipelines
+
 --------
 
 ## Installation
 
+`npm i -g shipe`, or use on the fly with `npx shipe`
+
 ## Usage
+
+`echo foo | shipe 'a => a.trim() + "bar"'` => `foobar`
 
 ## License
 
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 
-const main = () => {
+const id = (a) => a
+const script = eval(process.argv[2]) || id
 
+const handle = (handlers) => {
+  process.stdin.resume()
+  process.stdin.on('data', (d) => {
+    process.stdout.write(handlers(d.toString()))
+    process.stdout.write('\n')
+  })
+  process.stdout.on('error', function epipeFilter(err) {
+    if (err.code === 'EPIPE') return process.exit()
+    if (process.stdout.listeners('error').length <= 1) {
+      process.stdout.removeAllListeners()
+      process.stdout.emit('error', err)
+      process.stdout.on('error', epipeFilter)
+    }
+  })
+}
+
+const main = () => {
+  handle(script)
 }
 
 if (!module.parent) main()
