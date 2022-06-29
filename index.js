dfcef32fd6cfa8ce9355fe30e8eaf0045fc6a150
zacanger <zac@zacanger.com>
2022-06-29 15:11:59 -0600

    chore: updates

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-require('./lib')
+const handle = (handlers) => {
+  process.stdin.resume()
+  process.stdin.on('data', (d) => {
+    process.stdout.write(handlers(d.toString()))
+  })
+
+  process.stdout.on('error', function errorHandler (err) {
+    if (err.code === 'EPIPE') {
+      return process.exit()
+    }
+    if (process.stdout.listeners('error').length <= 1) {
+      process.stdout.removeAllListeners()
+      process.stdout.emit('error', err)
+      process.stdout.on('error', errorHandler)
+    }
+  })
+}
+
+// eslint-disable-next-line no-eval
+handle(eval(process.argv[2]) || ((a) => a))
